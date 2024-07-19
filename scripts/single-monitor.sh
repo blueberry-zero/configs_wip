8bfd421fad7d7193894b8d2e0b7aad8c66ce6c15
Saad Khan <khan.ssjc@gmail.com>
2024-07-18 20:43:14 -0400

    feat: multi monitor scripts

---
+++ scripts/single-monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Monitor names (replace with your actual monitor names from `xrandr`)
+laptop_monitor="eDP-1"
+left_monitor="DP-1-1.1"
+right_monitor="DP-1-1.2"
+
+# Turn on laptop monitor 
+xrandr --output "$laptop_monitor" --auto
+
+# Disable external monitors
+xrandr --output "$left_monitor" --off --output "$right_monitor" --off
+
