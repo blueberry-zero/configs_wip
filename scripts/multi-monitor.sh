ef153f40682664e5dc5df1c61fef9bdc4c2a48be
Saad Khan <khan.ssjc@gmail.com>
2024-12-12 20:21:21 -0500

    transfer

--- scripts/multi-monitor.sh
+++ scripts/multi-monitor.sh
@@ -2,9 +2,11 @@
 
 # Monitor names (defaults)
 laptop_monitor="eDP-1"
-left_monitor="${1:-DP-1-1.1}"  # Use 1st argument or default
-right_monitor="${2:-DP-1-1.2}" # Use 2nd argument or default
+# left_monitor="${1:-DP-1-1.1}"  # Use 1st argument or default
+# right_monitor="${2:-DP-1-1.2}" # Use 2nd argument or default
 
+left_monitor="${1:-DP-1-0.1}"  # Use 1st argument or default
+right_monitor="${2:-DP-1-0.2}" # Use 2nd argument or default
 
 # Configure external monitors
 xrandr \
--- scripts/single-monitor.sh
+++ scripts/single-monitor.sh
@@ -2,8 +2,10 @@
 
 # Monitor names (replace with your actual monitor names from `xrandr`)
 laptop_monitor="eDP-1"
-left_monitor="DP-1-1.1"
-right_monitor="DP-1-1.2"
+# left_monitor="DP-1-1.1"
+# right_monitor="DP-1-1.2"
+left_monitor="DP-1-0.1"
+right_monitor="DP-1-0.2"
 
 # Turn on laptop monitor 
 xrandr --output "$laptop_monitor" --auto
